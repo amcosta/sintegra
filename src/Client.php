3b4870b25c521d1a7b6633c9b3727d9b7c2d12cd
Alex Moreno <alex.moreno.costa@gmail.com>
2018-12-07 12:55:37 -0200

    Primeiro commit

---
+++ src/Client.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Sintegra;
+
+class Client
+{
+    private $client;
+
+    public function __construct()
+    {
+    }
+
+    public function searchByCNPJ(string $cnpj)
+    {
+
+    }
+
+    public function searchByIE(string $ie)
+    {
+
+    }
+}
